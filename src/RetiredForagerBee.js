43aac648a33717e475a41c211c9d667a428b5d95
khengschew <khengc@gmail.com>
2019-01-13 20:03:03 -0800

    Implement RetiredForagerBee class that inherits from ForagerBee

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,14 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor (canFly = false, job = 'gamble', age = 40, color = 'grey') {
+    super(canFly, job, age, color);
+    this.treasureChest = [];
+  }
+
+  forage () {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble (treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
