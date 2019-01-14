079ca00884ee331f82d3374fff80358eefb6550d
khengschew <khengc@gmail.com>
2019-01-13 20:02:44 -0800

    Implement ForagerBee class that inherits from Bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,11 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor (canFly = true, job = 'find pollen', age = 10, color) {
+    super(job, age, color);
+    this.canFly = canFly;
+    this.treasureChest = [];
+  }
+
+  forage (treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
